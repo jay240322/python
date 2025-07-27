cac0ed56704d88ddc902ffff69274f9403fb0099
jay patel <patel.jay.brij03@gmail.com>
2025-07-27 17:51:25 +0530

    change

--- unit1/5.py
+++ unit1/5.py
@@ -2,11 +2,14 @@
 list1 = [10,20,30,40]
 list2 = [40,50,60,10]
 
-for item in list1:
-    if item in list2:
-        print(item,"is common element")
-    else:
-        print(item,"non common element")
+# for item in list1:
+#     if item in list2:
+#         print(item,"is common element")
 # for item in list1:
 #     if item not in list2:
 #         print(item,"are not in element")
+for item in list1:
+    if item in list2:
+        print(item,"this is comman element")
+    else:
+        print(item,"this is not common element")
