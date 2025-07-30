3a0aa6d815a8af003f5309aca260873cbceaf716
jay patel <patel.jay.brij03@gmail.com>
2025-07-30 17:56:38 +0530

    done

--- unit1/6.py
+++ unit1/6.py
@@ -1,8 +1,17 @@
-a = 25
-b = 25
+# a = 25
+# b = 25
+# print(id(a))
+# print(id(b))
+
+# if(a is b):
+#     print("a and b have same identity")
+# else:
+#     print("a and b havenot same identity")
+
+a = int(input("enter first number in two digit:"))
+b = int(input("enter second  number in two digit:"))
 print(id(a))
 print(id(b))
-
 if(a is b):
     print("a and b have same identity")
 else:
---
+++ unit1/7.py
@@ -0,0 +1,3 @@
+# write a program that evaluates an expression given by user at run time using eval() function 
+x = eval(input("Enter an expression:"))
+print("result=%d"%x)
