d1bdfa4fb8a33f8a33f8af9ee0061a0f5f6f05f7
jay patel <patel.jay.brij03@gmail.com>
2025-09-03 19:19:23 +0530

    done

--- unit1/10.py
+++ unit1/10.py
@@ -1,7 +1,7 @@
 # search an element in list using forloop
 group1 = [1,2,3,4,5,6]
 search = int(input("enter a number to search:"))
-for i in group1:
+for i in group1: 
     if search == i:
         print("found ",search)
         break
--- unit2/9.py
+++ unit2/9.py
@@ -1,4 +1,4 @@
-# lambda anonymous sunction to find bigger number in two given number
+# lambda anonymous function to find bigger number in two given number
 max = lambda x,y: x if x>y else y
 a = int(input("enter the first number:"))
 b = int(input("enter the second number:"))
