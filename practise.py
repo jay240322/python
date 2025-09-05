d2bf42fa66c727766a309f67d1c109dfa1ceebcd
jay patel <patel.jay.brij03@gmail.com>
2025-09-05 22:37:00 +0530

    done

--- practise.py
+++ practise.py
@@ -1,25 +1,7 @@
 from array import *
+x = array('i',[10,20,30,40,50,60,60])
+n = len(x)
+for i in range(n):
+    print(x[i],end=" ")
 
-array = array('i',[10,20,30,40,50])
-print("original array:",array)
-
-array.append(60)
-print("after appending 60:",array)
-
-array.insert(1,99)
-print("after inserting 99 :",array)
-
-array.remove(20)
-print("after removing 20",array)
-
-n = array.pop()
-print("after poping the element:",array)
-print("poped element:",n)
-
-n = array.index(20)
-print("the index of 30 is:",n)
-
-lst = array.tolist()
-print("list:",lst)
-print("array:",array)
-
+print
